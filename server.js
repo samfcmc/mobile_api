4c821b0dc3a4d4e23e1ef28b196d141afcbda1d1
Samuel Coelho <samuelfcmc@gmail.com>
2013-11-09 18:21:50 +0000

    Added package.json file to define dependencies

--- server.js
+++ server.js
@@ -3,3 +3,16 @@
    */
 
 console.log("Olá");
+
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+	  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+	  console.log("Listening on " + port);
+});
